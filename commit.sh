5f4027c84c13cb716c9e2de30c541bc59775990c
Zeng Guanning <zgn21@mails.tsinghua.edu.cn>
2023-11-23 18:27:22 +0800

    自动提交

--- commit.sh
+++ commit.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-conda activate RISCV
-git add .
-git commit -m "自动提交"
-git push origin master
-mkdocs gh-deploy
+
+# Activate the conda environment and run subsequent commands
+/home/guanning/miniconda3/condabin/conda run -n RISCV git add .
+/home/guanning/miniconda3/condabin/conda run -n RISCV git commit -m "自动提交"
+/home/guanning/miniconda3/condabin/conda run -n RISCV git push origin master
+/home/guanning/miniconda3/condabin/conda run -n RISCV mkdocs gh-deploy
