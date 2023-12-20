7487e80a712a699f32ee59016a8b05be93e45cba
Guanning <zgn21@mails.tsinghua.edu.cn>
2023-12-21 02:58:58 +0800

    自动提交

--- commit.sh
+++ commit.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-CONDA_PATH=$(conda info --base)
-source $CONDA_PATH/etc/profile.d/conda.sh
-conda init
-conda activate base
 pip install mkdocs mkdocs-material
 git add .
 git commit -m "自动提交"
